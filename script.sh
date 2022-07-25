c2049c99dbad0c45389a3766979d03e2fb1dd54d
alfons caroles <alfons.caroles@rocketlab.com.au>
2022-07-25 17:31:26 +1000

    Script  could be run in both linux and mac

--- script.sh
+++ script.sh
@@ -1,117 +1,117 @@
-# need PlistBuddy installed
 appName=ActionApp
 appTestName="${appName}Tests"
-currentIOSPatchVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleVersion/{getline; print}' | awk -F "[<>]" '{print $3}')
-currentIOSVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleShortVersionString/{getline; print}' | awk -F "[<>]" '{print $3}')
-currentAndroidVersion=$(cat android/app/build.gradle | grep -m1 'versionName' | cut -d '"' -f2)
-currentVersionCode=$(cat android/app/build.gradle | grep -m1 'versionCode' | tr -s ' ' | cut -d ' ' -f3)
 
-currentIOSFullVersion="$currentIOSVersion.$currentIOSPatchVersion"
-currentIOSMajorVersion=${currentIOSVersion%.*}
-currentIOSMinorVersion=${currentIOSVersion##*.}
+get_android_version() {
+    currentAndroidVersion=$(cat android/app/build.gradle | grep -m1 'versionName' | cut -d '"' -f2)
+    currentVersionCode=$(cat android/app/build.gradle | grep -m1 'versionCode' | tr -s ' ' | cut -d ' ' -f3)
 
-echo "current ios major version: $currentIOSMajorVersion"
-echo "current ios minor version: $currentIOSMinorVersion"
-echo "current ios patch version: $currentIOSPatchVersion"
-echo "current ios version: $currentIOSFullVersion"
-echo "current android version: $currentAndroidVersion"
-echo "current android version code: $currentVersionCode"
-
-increaseAndroidVersion() {
-    newAndroidVersionCode=$(awk -F. '{ print ($1*10000000)+($2*100000+$3) }' <<< $1)
-    # Set versionName and versionCode in app build.gradle for 3 digits
-    sed -i -E 's/versionName "([0-9]+.[0-9]+.[0-9]+)"/versionName "'$1'"/' android/app/build.gradle
-    # Works for code with 8 digits or less
-    sed -i -E "s/versionCode [0-9]{0,8}$/versionCode $newAndroidVersionCode/" android/app/build.gradle
-    echo "version name: $1"
-    echo "version code: $newAndroidVersionCode"
+    echo "current android version: $currentAndroidVersion"
+    echo "current android version code: $currentVersionCode"
 }
 
-increaseIOSVersion() {
+get_ios_version() {
+    currentIOSPatchVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleVersion/{getline; print}' | awk -F "[<>]" '{print $3}')
+    currentIOSVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleShortVersionString/{getline; print}' | awk -F "[<>]" '{print $3}')
+
+    currentIOSFullVersion="$currentIOSVersion.$currentIOSPatchVersion"
+    currentIOSMajorVersion=${currentIOSVersion%.*}
+    currentIOSMinorVersion=${currentIOSVersion##*.}
+
+    echo "current ios version: $currentIOSFullVersion"
+}
+
+increase_android_version() {
+    newAndroidVersionCode=$(awk -F. '{ print ($1*10000000)+($2*100000+$3) }' <<< $1)
+    # Set versionName and versionCode in app build.gradle for 3 digits
+    sed -i '.bak' -E 's/versionName "([0-9]+.[0-9]+.[0-9]+)"/versionName "'$1'"/' android/app/build.gradle
+    # Works for code with 8 digits or less
+    sed -i '.bak' -E "s/versionCode [0-9]{0,8}$/versionCode $newAndroidVersionCode/" android/app/build.gradle
+
+    rm android/app/build.gradle.bak
+    echo "Updated Android Version Name: $1"
+    echo "Updated Android Version Code: $newAndroidVersionCode"
+}
+
+increase_ios_version() {
     newVersion=$1
     semver=( ${newVersion//./ } )
     updatedShortVersion="${semver[0]}.${semver[1]}"
     updatedPatchVersion="${semver[2]:-0}"
     # Set IOS info.plist
-    sed -i '' -E "/<key>CFBundleVersion<\/key>/{n;s/<string>[0-9]<\/string>/<string>$updatedPatchVersion<\/string>/;}" ios/$appName/info.plist
-    sed -i '' -E "/<key>CFBundleShortVersionString<\/key>/{n;s/<string>[0-9]+(\.[0-9]+)*<\/string>/<string>$updatedShortVersion<\/string>/;}" ios/$appName/info.plist
-    sed -i '' -E "/<key>CFBundleVersion<\/key>/{n;s/<string>[0-9]<\/string>/<string>$updatedPatchVersion<\/string>/;}" ios/$appTestName/info.plist
-    sed -i '' -E "/<key>CFBundleShortVersionString<\/key>/{n;s/<string>[0-9]+(\.[0-9]+)*<\/string>/<string>$updatedShortVersion<\/string>/;}" ios/$appTestName/info.plist
+    sed -i '.bak' -E "/<key>CFBundleVersion<\/key>/{n;s/<string>([0-9]+)*<\/string>/<string>$updatedPatchVersion<\/string>/;}" ios/$appName/info.plist
+    sed -i '.bak' -E "/<key>CFBundleShortVersionString<\/key>/{n;s/<string>[0-9]+(\.[0-9]+)*<\/string>/<string>$updatedShortVersion<\/string>/;}" ios/$appName/info.plist
+    sed -i '.bak' -E "/<key>CFBundleVersion<\/key>/{n;s/<string>([0-9]+)*<\/string>/<string>$updatedPatchVersion<\/string>/;}" ios/$appTestName/info.plist
+    sed -i '.bak' -E "/<key>CFBundleShortVersionString<\/key>/{n;s/<string>[0-9]+(\.[0-9]+)*<\/string>/<string>$updatedShortVersion<\/string>/;}" ios/$appTestName/info.plist
     
     # Set initial version in IOS pbxproj
-    sed -i '' -e "s/CURRENT_PROJECT_VERSION \= [^\;]*\;/CURRENT_PROJECT_VERSION = $updatedPatchVersion;/" ios/${appName}.xcodeproj/project.pbxproj
+    sed -i '.bak' -E "s/CURRENT_PROJECT_VERSION \= [^\;]*\;/CURRENT_PROJECT_VERSION = $updatedPatchVersion;/" ios/$appName.xcodeproj/project.pbxproj
 
-    echo "new version: $1"
-    echo "updated short version: $updatedShortVersion"
-    echo "updated patch version: $updatedPatchVersion"
+    rm ios/$appName/info.plist.bak ios/$appTestName/info.plist.bak ios/$appName.xcodeproj/project.pbxproj.bak
+    echo "Updated IOS Version: $1"
 }
 
-# setInitialVersion() {
-#     # Set initial version name
-#     sed -i '' -E 's/versionName "[0-9]"/versionName "1.0.0"/' android/app/build.gradle
-#     # Set initial version code
-#     sed -i '' -E "s/versionCode [0-9]$/versionCode 10000000/" android/app/build.gradle
-#     # Set initial version in IOS info.plist
-#     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion 1" ios/$appName/info.plist
-#     /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 1.1" ios/$appName/info.plist
-#     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion 1" ios/$appTestName/info.plist
-#     /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 1.1" ios/$appTestName/info.plist
-#     # Set initial version in IOS pbxproj
-#     sed -i '' -e "s/CURRENT_PROJECT_VERSION \= [^\;]*\;/CURRENT_PROJECT_VERSION = 1;/" ios/${appName}.xcodeproj/project.pbxproj
-# }
+increase_build_selection() {
+    currentIOSPatchVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleVersion/{getline; print}' | awk -F "[<>]" '{print $3}')
+    currentIOSVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleShortVersionString/{getline; print}' | awk -F "[<>]" '{print $3}')
+    currentIOSFullVersion="$currentIOSVersion.$currentIOSPatchVersion"
+    currentIOSMajorVersion=${currentIOSVersion%.*}
 
-# increaseBuildNumber() {
-#     if [ $1 == 'patch' ]; then
-#         newPatchVersion=$(($currentIOSPatchVersion + 1))
-#         currentMinorVersionCode=$(($currentIOSMinorVersion * 1000))
+    if [ $1 = 'major' ]; then
+        newIOSMajorVersion=$(($currentIOSMajorVersion + 1))
+        # Reset minor and patch to zero when major version updated
+        updatedVersion="$newIOSMajorVersion.0.0"
 
-#         # Set patch version in IOS info.plist
-#         # /usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${newPatchVersion}'" ios/$appName/info.plist
+        increaseAndroidVersion $updatedVersion
+        increaseIOSVersion $updatedVersion
 
-#         # Set patch version in IOS pbxproj
-#         # sed -i '' -e "s/CURRENT_PROJECT_VERSION \= [^\;]*\;/CURRENT_PROJECT_VERSION = ${newPatchVersion};/" ios/${appName}.xcodeproj/project.pbxproj
+        echo "New Build Version $updatedVersion"
+    elif [ $1 = 'minor' ]; then
+        currentIOSMinorVersion=${currentIOSVersion##*.}
+        newIOSMinorVersion=$(($currentIOSMinorVersion + 1))
+        # Reset patch to zero when minor version updated
+        updatedVersion="$currentIOSMajorVersion.$newIOSMinorVersion.0"
 
-#         # Set versionName and versionCode in app build.gradle
-#         sed -i '' -E 's/versionName "[0-9]?.[0-9]?.[0-9]"/versionName "'$currentIOSMajorVersion.$currentIOSMinorVersion.$newPatchVersion'"/' android/app/build.gradle
-#         sed -i '' -E "s/versionCode [0-9]{0,8}$/versionCode $currentIOSMajorVersion$currentMinorVersionCode$newPatchVersion/" android/app/build.gradle
+        increaseAndroidVersion $updatedVersion
+        increaseIOSVersion $updatedVersion
 
-#         echo "new patch version $newPatchVersion"
-#         echo "test patch $testPatch"
-#     elif [ $1 == 'major' ]; then
-#         newIOSMajorVersion=$(($currentIOSMajorVersion + 1))
-#         newIOSVersion="$newIOSMajorVersion.$currentIOSMinorVersion"
-#         /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString '${newIOSVersion}'" ios/$appName/info.plist
-#         echo "new Version $newIOSVersion"
-#     else
-#         # newIOSMinorVersion=$(($currentIOSMinorVersion + 1))
-#         # newIOSVersion="$currentIOSMajorVersion.$newIOSMinorVersion"
-#         # /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString '${newIOSVersion}'" ios/$appName/info.plist
-#         echo "please specify an increment version type"
-#     fi
-# }
+        echo "New Build Version $updatedVersion"
+    elif [ $1 = 'patch' ]; then
+        newPatchVersion=$(($currentIOSPatchVersion + 1))
+        updatedVersion="$currentIOSVersion.$newPatchVersion"
 
-increaseIOSVersion "4.1"
-increaseAndroidVersion "4.1"
+        increaseAndroidVersion $updatedVersion
+        increaseIOSVersion $updatedVersion
 
-updatedIOSPatchVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleVersion/{getline; print}' | awk -F "[<>]" '{print $3}')
-updatedIOSVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleShortVersionString/{getline; print}' | awk -F "[<>]" '{print $3}')
-updatedAndroidVersionCode=$(cat android/app/build.gradle | grep -m1 'versionCode' | tr -s ' ' | cut -d ' ' -f3)
-updatedAndroidVersion=$(cat android/app/build.gradle | grep -m1 'versionName' | cut -d '"' -f2)
-updatedIOSFullVersion="$updatedIOSVersion.$updatedIOSPatchVersion"
-echo "updated ios version: $updatedIOSFullVersion"
-echo "updated android version code: $updatedAndroidVersionCode"
-echo "updated android version name: $updatedAndroidVersion"
-# getVersion() {
-#     currentIOSFullVersion="$currentIOSVersion.$currentIOSPatchVersion"
-#     echo "current ios version: $currentIOSFullVersion"
-#     echo "current android version: $currentAndroidVersion"
-#     # [[ $currentIOSVersion == "1.0" ]] && echo "ios version got" || echo "ios version failed"
-#     # [[ $currentAndroidVersion == "1.0" ]] && echo "android version got" || echo "android version failed"
-#     if [ $currentAndroidVersion = $currentIOSFullVersion ]; then
-#         echo "true"
-#     else
-#         echo "false"
-#     fi
-# }
+        echo "New Build Version $updatedVersion"
+    else
+        echo "Please input the increment type"
+    fi
+}
 
-# getVersion
+increase_app_version() {
+    currentIOSPatchVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleVersion/{getline; print}' | awk -F "[<>]" '{print $3}')
+    currentIOSVersion=$(cat ios/ActionApp/info.plist | awk '/CFBundleShortVersionString/{getline; print}' | awk -F "[<>]" '{print $3}')
+    currentAndroidVersion=$(cat android/app/build.gradle | grep -m1 'versionName' | cut -d '"' -f2)
+    currentIOSFullVersion="$currentIOSVersion.$currentIOSPatchVersion"
+
+    echo "current ios version: $currentIOSFullVersion"
+    echo "current android version: $currentAndroidVersion"
+
+    if [ $currentAndroidVersion = $currentIOSFullVersion ]; then
+        echo "version length: ${#currentAndroidVersion}"
+        if [ ${#currentAndroidVersion} -lt 5 ]; then
+            increaseAndroidVersion "1.0.0"
+            increaseIOSVersion "1.0.0"
+        else
+            increaseBuildSelection $1
+        fi
+    else
+        echo "Please fix mismatched IOS ${currentIOSFullVersion} and Android ${currentAndroidVersion} version"
+    fi
+}
+
+if [ "$1" = "set-android-version" ]; then increase_android_version $2; exit; fi
+if [ "$1" = "set-ios-version" ]; then increase_ios_version $2; exit; fi
+if [ "$1" = "increase-app-version" ]; then increase_app_version $2; exit; fi
+if [ "$1" = "get-android-version" ]; then get_android_version; exit; fi
+if [ "$1" = "get-ios-version" ]; then get_ios_version; exit; fi
